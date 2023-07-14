15caa326d97ab296adefe754bb5762dc2596b014
Chris Gibson <c.gibson@nchain.com>
2023-07-14 10:52:40 +0100

    pointers.cpp

---
+++ pointers.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+void print(auto p, const char* s)
+{ 
+    cout << "// " << s << '\n';
+
+    cout << "p: address of p\t\t" << p << endl; // decay to pointer to first element
+    cout << "&p: address of p\t" << &p << endl;
+    cout << "&p[0]: addr of elem 0\t" << &p[0] << endl;
+    cout << "&p[1]: addr of elem 1\t" << &p[1] << endl;
+    cout << "&p[2]: addr of elem 2\t" << &p[2] << endl;
+    cout << "*p: value of elem 0\t" << *p << endl;
+    cout << "p[0]: value of elem 0\t" << p[0] << endl;
+    cout << "p[1]: value of elem 1\t" << p[1] << endl;
+    cout << "p[2]: value of elem 2\t" << p[2] << endl;
+    cout << endl;
+}
+
+int main()
+{
+    int a[] = {1, 2, 3};
+    print(a, "int a[]");
+
+    int* p = a; // implicit conversion, pointer decay
+    print(p, "int*");
+
+    int(*pa)[3] = &a;
+    print(pa, "int(*pa)[]");
+
+    pa = nullptr;
+    print(pa, "int(*pa)[] = nullptr");
+}
